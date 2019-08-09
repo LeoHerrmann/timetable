636d2de0ab87c2da509ed4d21f89869b89859fac
leo <familiehn.leo@gmail.com>
2019-08-09 21:35:22 +0200

    translation

--- configuration.js
+++ configuration.js
@@ -3,15 +3,15 @@ var config = {
 		{
 			day: "Montag",
 			schedule: [
-				{subject: "Physik", room: 321},
-				{subject: "Erdkunde", room:304},
-				{subject: "Deutsch", room: 329},
-				{subject: "Bili", room: 304},
-				{subject: "Biologie", room:113},
-				{subject: "Informatik", room:312}, 
+				{subject: "Physics", room: 321},
+				{subject: "Geography", room:304},
+				{subject: "German", room: 329},
+				{subject: "History", room: 304},
+				{subject: "Biology", room:113},
+				{subject: "Computer Science", room:312}, 
 				{subject: "", room: ""},
-				{subject: "Mathe", room:329},
-				{subject: "Musik", room:211},
+				{subject: "Maths", room:329},
+				{subject: "Music", room:211},
 				{subject: "", room: ""}
 			]
 		},
@@ -19,13 +19,13 @@ var config = {
 		{
 			day: "Dienstag",
 			schedule: [
-				{subject: "Informatik", room: 312},
-				{subject: "Deutsch", room: 208},
-				{subject: "Mathe", room: 207},
+				{subject: "Computer Science", room: 312},
+				{subject: "German", room: 208},
+				{subject: "Maths", room: 207},
 				{subject: "", room: ""},
-				{subject: "Englisch", room: 222},
-				{subject: "Englisch", room: 222},
-				{subject: "Musik",  room: 211},
+				{subject: "English", room: 222},
+				{subject: "English", room: 222},
+				{subject: "Music",  room: 211},
 				{subject: "", room: ""},
 				{subject: "", room: ""},
 				{subject: "", room: ""}
@@ -35,12 +35,12 @@ var config = {
 		{
 			day: "Mittwoch",
 			schedule: [
-				{subject: "Erdkunde", room:306},
+				{subject: "Geography", room:306},
 				{subject: "Religion", room:209},
-				{subject: "Englisch", room:203},
-				{subject: "Mathe", room:111},
-				{subject: "Biologie", room:113},
-				{subject: "Physik", room:321},
+				{subject: "English", room:203},
+				{subject: "Maths", room:111},
+				{subject: "Biology", room:113},
+				{subject: "Physics", room:321},
 				{subject: "", room: ""},
 				{subject: "", room: ""},
 				{subject: "", room: ""},
@@ -51,13 +51,13 @@ var config = {
 		{
 			day: "Donnerstag",
 			schedule: [
-				{subject: "Physik", room:321},
-				{subject: "Bili", room:304},
-				{subject: "Informatik", room:312},
-				{subject: "Informatik", room:312},
-				{subject: "Englisch", room:312},
+				{subject: "Physics", room:321},
+				{subject: "History", room:304},
+				{subject: "Computer Science", room:312},
+				{subject: "Computer Science", room:312},
+				{subject: "English", room:312},
 				{subject: "", room:""},
-				{subject: "Musik", room:214},
+				{subject: "Music", room:214},
 				{subject: "Religion", room:315},
 				{subject: "", room: ""},
 				{subject: "", room: ""}
@@ -67,17 +67,16 @@ var config = {
 		{
 			day: "Freitag",
 			schedule: [
-				{subject: "Physik", room:321},
-				{subject: "Physik", room:321},
-				{subject: "Deutsch", room:206},
-				{subject: "Biologie", room:113},
-				{subject: "Informatik", room:216},
-				{subject: "Englisch", room:217},
+				{subject: "Physics", room:321},
+				{subject: "Physics", room:321},
+				{subject: "German", room:206},
+				{subject: "Biology", room:113},
+				{subject: "Computer Science", room:216},
+				{subject: "English", room:217},
 				{subject: "Sport", room:"SHG"},
 				{subject: "Sport", room:"SHG"},
 				{subject: "", room: ""},
-				{subject: "", room: ""}
-			]
+				{subject: "", room: ""}			]
 		}
 	],
 	
@@ -99,133 +98,16 @@ var config = {
 	
 	
 	colors: {
-		Bili:			"#520",
-		Biologie: 		"#373",
-		Deutsch:		"#05A",
-		Englisch: 		"#F80",
-		Informatik:		"#477",
-		Mathe: 			"#092",
-		Musik: 			"#D22",
-		Physik: 		"#08F",
-		Religion: 		"#A0D",
-		Erdkunde:		"#FC0",
-		Sport: 			"#222",	
+		"History":			"#520",
+		"Biology": 			"#373",
+		"German":			"#05A",
+		"English": 			"#F80",
+		"Computer Science":	"#477",
+		"Maths": 			"#092",
+		"Music": 			"#D22",
+		"Physics": 			"#08F",
+		"Religion": 		"#A0D",
+		"Geography":		"#FC0",
+		"Sport":			"#222",
 	}
 };
-/*var timetable = [
-	{
-		day: "Montag",
-		schedule: [
-			{subject: "Physik", room: 321},
-			{subject: "Erdkunde", room:304},
-			{subject: "Deutsch", room: 329},
-			{subject: "Bili", room: 304},
-			{subject: "Biologie", room:113},
-			{subject: "Informatik", room:312}, 
-			{subject: "", room: ""},
-			{subject: "Mathe", room:329},
-			{subject: "Musik", room:211},
-			{subject: "", room: ""}
-		]
-	},
-
-	{
-		day: "Dienstag",
-		schedule: [
-			{subject: "Informatik", room: 312},
-			{subject: "Deutsch", room: 208},
-			{subject: "Mathe", room: 207},
-			{subject: "", room: ""},
-			{subject: "Englisch", room: 222},
-			{subject: "Englisch", room: 222},
-			{subject: "Musik",  room: 211},
-			{subject: "", room: ""},
-			{subject: "", room: ""},
-			{subject: "", room: ""}
-		]
-	},
-
-	{
-		day: "Mittwoch",
-		schedule: [
-			{subject: "Erdkunde", room:306},
-			{subject: "Religion", room:209},
-			{subject: "Englisch", room:203},
-			{subject: "Mathe", room:111},
-			{subject: "Biologie", room:113},
-			{subject: "Physik", room:321},
-			{subject: "", room: ""},
-			{subject: "", room: ""},
-			{subject: "", room: ""},
-			{subject: "", room: ""}
-		]
-	},
-	
-	{
-		day: "Donnerstag",
-		schedule: [
-			{subject: "Physik", room:321},
-			{subject: "Bili", room:304},
-			{subject: "Informatik", room:312},
-			{subject: "Informatik", room:312},
-			{subject: "Englisch", room:312},
-			{subject: "", room:""},
-			{subject: "Musik", room:214},
-			{subject: "Religion", room:315},
-			{subject: "", room: ""},
-			{subject: "", room: ""}
-		]
-	},
-	
-	{
-		day: "Freitag",
-		schedule: [
-			{subject: "Physik", room:321},
-			{subject: "Physik", room:321},
-			{subject: "Deutsch", room:206},
-			{subject: "Biologie", room:113},
-			{subject: "Informatik", room:216},
-			{subject: "Englisch", room:217},
-			{subject: "Sport", room:"SHG"},
-			{subject: "Sport", room:"SHG"},
-			{subject: "", room: ""},
-			{subject: "", room: ""}
-		]
-	}
-];
-
-
-
-
-
-var periods = [
-	{start: "07:50", end: "08:35"},
-	{start: "08:40", end: "09:25"},
-	{start: "09:40", end: "10:25"},
-	{start: "10:30", end: "11:15"},
-	{start: "11:30", end: "12:15"},
-	{start: "12:15", end: "13:00"},
-	{start: "13:15", end: "14:00"},
-	{start: "14:00", end: "14:45"},
-	{start: "14:45", end: "15:30"},
-	{start: "15:30", end: "16:15"}
-];
-
-
-
-
-
-var colors = {
-	Bili:			"#520",
-	Biologie: 		"#373",
-	Deutsch:		"#05A",
-	Englisch: 		"#F80",
-	Informatik:		"#477",
-	Mathe: 			"#092",
-	Musik: 			"#D22",
-	Physik: 		"#08F",
-	Religion: 		"#A0D",
-	Erdkunde:		"#FC0",
-	Sport: 			"#222",
-};
-*/
