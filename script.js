72baa3e4fc426d0ecc5bd247f9608a7d5bcfcc1e
leo <familiehn.leo@gmail.com>
2019-08-16 18:11:31 +0200

    Keyboard events

--- script.js
+++ script.js
@@ -1,119 +1,122 @@
 window.onload = function() {
-	create_header_buttons();
-	fill_periods_container();
-	create_subject_divs();
-	show_current_timetable();
-	add_swipe_events();
+	dom_setup();
+	add_navigation_events();
+	timetable.display_current();
 
 
 
-	function create_header_buttons() {
-		var header = document.getElementsByTagName("header")[0];
-		var counter = 0;
+	function dom_setup() {
+		create_header_buttons();
+		fill_periods_container();
+		create_subject_divs();
 		
 		
-		for (entry of config.timetable) {
-			var newButton = document.createElement("button");
+		function create_header_buttons() {
+			var header = document.getElementsByTagName("header")[0];
+			var counter = 0;
+		
+		
+			for (entry of config.timetable) {
+				var newButton = document.createElement("button");
 			
-			newButton.innerText = entry.day.substring(0,2);
-			newButton.setAttribute("data-dayNumber", counter);
+				newButton.innerText = entry.day.substring(0,2);
+				newButton.setAttribute("data-dayNumber", counter);
 			
 			
-			newButton.addEventListener("click", function(e) {
-				var headerButtons = document.querySelectorAll("header > button");
+				newButton.addEventListener("click", function(e) {
+					var headerButtons = document.querySelectorAll("header > button");
 			
-				for (button of headerButtons) {
-					button.classList.remove("open");
-				}
+					for (button of headerButtons) {
+						button.classList.remove("open");
+					}
 
-				e.target.classList.add("open");
+					e.target.classList.add("open");
 
-				timetable.display_for_day(e.target.getAttribute("data-dayNumber"));
-			});
+					timetable.display_for_day(e.target.getAttribute("data-dayNumber"));
+				});
 			
 			
-			header.append(newButton);
-			counter++;
+				header.append(newButton);
+				counter++;
+			}
 		}
-	}
 
 
+		function fill_periods_container() {
+			for (period of config.periods) {
+				var periodDiv = document.createElement("div");
+				var startSpan = document.createElement("span");
+				var endSpan = document.createElement("span");
 
-	function fill_periods_container() {
-		for (period of config.periods) {
-			var periodDiv = document.createElement("div");
-			var startSpan = document.createElement("span");
-			var endSpan = document.createElement("span");
+				startSpan.innerText = period.start;
+				endSpan.innerText = period.end;
 
-			startSpan.innerText = period.start;
-			endSpan.innerText = period.end;
-
-			periodDiv.append(startSpan, endSpan);
-			periodDiv.classList.add("period");
-			document.getElementById("periods_container").append(periodDiv);
+				periodDiv.append(startSpan, endSpan);
+				periodDiv.classList.add("period");
+				document.getElementById("periods_container").append(periodDiv);
+			}
 		}
-	}
 
 
+		function create_subject_divs() {
+			var subjectsContainer = document.getElementById("subjects_container");
 
-	function create_subject_divs() {
-		var subjectsContainer = document.getElementById("subjects_container");
+			for (let i = 0; i < config.periods.length; i++) {
+				var subjectDiv = document.createElement("div");
 
-		for (let i = 0; i < config.periods.length; i++) {
-			var subjectDiv = document.createElement("div");
+				subjectDiv.classList.add("subject");
+				subjectDiv.classList.add("hidden");
+				subjectDiv.style.transitionDelay = i*0.025 + "s";
 
-			subjectDiv.classList.add("subject");
-			subjectDiv.classList.add("hidden");
-			subjectDiv.style.transitionDelay = i*0.025 + "s";
+				subjectNameSpan	= document.createElement("span");
+				subjectRoomSpan	= document.createElement("span");
+				subjectDiv.append(subjectNameSpan);
+				subjectDiv.append(subjectRoomSpan);
 
-			subjectNameSpan	= document.createElement("span");
-			subjectRoomSpan	= document.createElement("span");
-			subjectDiv.append(subjectNameSpan);
-			subjectDiv.append(subjectRoomSpan);
-
-			subjectsContainer.append(subjectDiv);
-		}
-	}
-
-
-
-	function show_current_timetable() {
-		var d = new Date();
-		dayNumber = (d.getDay() != 0) ? d.getDay() - 1 : 6;
-
-		var headerButtons = document.querySelectorAll("header > button");
-
-		if (dayNumber >= config.timetable.length) {
-			headerButtons[0].classList.add("open");
-			timetable.display_for_day(0);
-		}
-		else {
-			headerButtons[dayNumber].classList.add("open");
-			timetable.display_for_day(dayNumber);
+				subjectsContainer.append(subjectDiv);
+			}
 		}
 	}
 	
 	
 	
-	function add_swipe_events() {
-		var timetable_div = document.getElementById("timetable");
+	function add_navigation_events() {
+		add_swipe_events();
+		add_keyboard_events();
+		
 	
-		timetable_div.ontouchstart = function(e_start) {
-			var touch_start_position = e_start.touches[0].clientX;
+		function add_swipe_events() {
+			var timetable_div = document.getElementById("timetable");
+	
+			timetable_div.ontouchstart = function(e_start) {
+				var touch_start_position = e_start.touches[0].clientX;
 
-			timetable_div.ontouchmove = function(e_end) {
-				var touch_end_position = e_end.touches[0].clientX;
+				timetable_div.ontouchmove = function(e_end) {
+					var touch_end_position = e_end.touches[0].clientX;
 
-				if (touch_start_position < touch_end_position - 75) {
-					timetable_div.ontouchmove = null;
+					if (touch_start_position < touch_end_position - 75) {
+						timetable_div.ontouchmove = null;
+						timetable.display_previous_day();
+					}
+					else if (touch_start_position > touch_end_position + 75) {
+						timetable_div.ontouchmove = null;
+						timetable.display_next_day();
+					}
+				};
+			};
+		}
+	
+	
+		function add_keyboard_events() {
+			window.addEventListener("keydown", function(e) {
+				if (e.keyCode == 37) {
 					timetable.display_previous_day();
 				}
-				else if (touch_start_position > touch_end_position + 75) {
-					timetable_div.ontouchmove = null;
+				else if (e.keyCode == 39) {
 					timetable.display_next_day();
 				}
-			};
-		};
+			});
+		}
 	}
 };
 
@@ -131,6 +134,23 @@ var timetable = {
 			timetable.subject_divs.fill(dayNumber);
 			timetable.subject_divs.show();
 		}, 450);
+	},
+	
+	
+	display_current: function() {
+		var d = new Date();
+		dayNumber = (d.getDay() != 0) ? d.getDay() - 1 : 6;
+
+		var headerButtons = document.querySelectorAll("header > button");
+
+		if (dayNumber >= config.timetable.length) {
+			headerButtons[0].classList.add("open");
+			timetable.display_for_day(0);
+		}
+		else {
+			headerButtons[dayNumber].classList.add("open");
+			timetable.display_for_day(dayNumber);
+		}
 	},
 	
 	
